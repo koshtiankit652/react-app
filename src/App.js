23c3977553eb1b880d311c9ad83218b2bbfb4fc5
koshtiankit652 <77959926+koshtiankit652@users.noreply.github.com>
2024-04-19 16:23:14 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Hello Guys this is react-app.
-          Hello Guys this is testing by K8S CICD\nHello Image latest issue resolved
+          Hello Guys this is testing by K8S CICD
         </p>
         <a
           className="App-link"
